bc34d071651e03bcee01b68fd5b48e814833804f
23380101076 <148200812+23380101076@users.noreply.github.com>
2024-01-11 00:30:44 +0300

    Update Program.cs

--- odev2/Program.cs
+++ odev2/Program.cs
@@ -5,20 +5,21 @@ namespace odev
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("KAÇ SAYI SIRALAMAK İSTERSİN ??");
+            Console.WriteLine("HELLO, WELCOME TO THE RANDOM NUMBER SORTING APPLICATION.\n OUR APPLICATION WILL ASK YOU 3 QUESTIONS AND EXPECT YOU TO ENTER THEM EXCLUSIVELY IN DIGITS.\n REMEMBER, THE NUMBER YOU WANT TO SORT SHOULD BE GREATER THAN 0. THE STARTING NUMBER SHOULD NOT BE GREATER THAN OR EQUAL TO THE ENDING NUMBER.\n THE RANGE YOU WANT TO SORT MUST BE COMPATIBLE WITH THE NUMBER OF ELEMENTS YOU WANT TO SORT.\n NOW, WE WILL ASK YOU HOW MANY NUMBERS YOU WANT TO SORT.");
+            Console.WriteLine("HOW MANY NUMBERS DO YOU WANT TO SORT??");
             int srlm = Int32.Parse(Console.ReadLine());
             if (srlm <= 0)
             {
-                Console.WriteLine("SIFIRDAN BÜYÜK VE RAKAMLARLA SIRALAMAK İSTEDİĞİN ADETİ DEKLARE ET");
+                Console.WriteLine("DECLARE THE NUMBER OF ELEMENTS TO SORT, WHICH SHOULD BE GREATER THAN ZERO AND CONSIST OF DIGITS");
                 return;
             }
-            Console.WriteLine("SIRALAMAK İSTEDİĞİNİZ SAYILAR HANGİ ARALIKTA BAŞLASIN ??");
+            Console.WriteLine("IN WHAT RANGE DO YOU WANT THE NUMBERS TO START ??");
             int strt = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("SIRALAMAK İSTEDİĞİNİZ SAYILAR HANGİ ARALIKTA BİTSİN ??");
+            Console.WriteLine("IN WHAT RANGE DO YOU WANT THE NUMBERS TO END ??");
             int end = Int32.Parse(Console.ReadLine());
             if (strt >= end)
             {
-                Console.WriteLine("BAŞLANGIÇ ARALAĞI BİTİŞ ARALIĞINDAN BÜYÜK OLAMAZ.\n LÜTFEN BAŞLANGIÇ SAYISI BİTİŞ SAYISINDAN KÜÇÜK OLACAK ŞEKİLDE GİRİŞ YAPINIZ.");
+                Console.WriteLine("THE STARTING RANGE CANNOT BE GREATER THAN THE ENDING RANGE.\nPLEASE ENTER THE STARTING NUMBER TO BE LESS THAN THE ENDING NUMBER.");
                 return;
             }
             Random random = new Random();
